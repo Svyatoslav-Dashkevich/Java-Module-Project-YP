0f17279b6d5c47c91ad38e3983faf26dc35ec299
Svyatoslav <79216334404@yandex.ru>
2025-03-16 23:02:45 +0300

    1st try

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        int speed = 0;
+        Race race = new Race();
+        Car[] cars = new Car[3];
+
+        for (int i = 1; i <= 3; i++) {
+                    System.out.println("Введите название автомобиля №" + i + ": ");
+                    name = scanner.next();
+
+                    while (true) {
+                        System.out.println("Введите скорость автомобиля №" + i + " от 0 до 250: ");
+                        speed = scanner.nextInt();
+                        if (speed > 0 && speed <= 250) {
+                        } else {
+                            System.out.println("Ошибка: скорость должна быть целым числом от 0 до 250. Попробуйте снова.");
+                        }
+                        break;
+                    }
+
+                    Car car = new Car(name, speed);
+                    race.newLeader(car);
+                    scanner.nextInt();
+                }
+
+                System.out.println("Самая быстрая машина: " + race.getLeader());
+
+                scanner.close();
+            }
+        }
+
