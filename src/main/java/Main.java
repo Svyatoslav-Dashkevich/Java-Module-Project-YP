e8ccf17e25737fe1e8b110efafbfe2b8c0602cad
Svyatoslav <79216334404@yandex.ru>
2025-03-17 21:46:32 +0300

    Much better, i hope)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,23 +4,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name;
-        int speed = 0;
+        int speed;
         Race race = new Race();
-        Car[] cars = new Car[3];
 
         for (int i = 1; i <= 3; i++) {
-                    System.out.println("Введите название автомобиля №" + i + ": ");
-                    name = scanner.next();
+            while (true) {
+                System.out.println("Введите название автомобиля №" + i + ": ");
+                name = scanner.nextLine().trim();
+                if (!name.isEmpty()) {
+                    break;
+                } else {
+                    System.out.println("Ошибка: название не может быть пустым. Попробуйте снова.");
+                }
+            }
 
-                    while (true) {
-                        System.out.println("Введите скорость автомобиля №" + i + " от 0 до 250: ");
-                        speed = scanner.nextInt();
-                        if (speed > 0 && speed <= 250) {
-                        } else {
-                            System.out.println("Ошибка: скорость должна быть целым числом от 0 до 250. Попробуйте снова.");
+            while (true) {
+                System.out.println("Введите скорость автомобиля №" + i + " от 0 до 250: ");
+                if (scanner.hasNextLine()) {
+                    String speedInput = scanner.nextLine().trim();
+                    if (!speedInput.isEmpty()) {
+                        try {
+                            speed = Integer.parseInt(speedInput);
+                            int maxSpeed = 250;
+                            int minSpeed = 0;
+                            if (speed >= minSpeed && speed <= maxSpeed) {
+                                break;
+                            } else {
+                                System.out.println("Ошибка: скорость должна быть целым числом от 0 до 250. Попробуйте снова.");
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.println("Ошибка: введите корректное целое число.");
                         }
-                        break;
+                    } else {
+                        System.out.println("Ошибка: скорость не может быть пустой. Попробуйте снова.");
                     }
+                }
+            }
 
                     Car car = new Car(name, speed);
                     race.newLeader(car);
